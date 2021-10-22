e0bad493aa9db2ca730f5570213971de5f0ff76b
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2021-10-22 11:52:23 +0330

    adding a simple test

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,1 +1,15 @@
 import pytest
+
+from r_logger import RLogger
+
+
+@pytest.fixture()
+def r_main_logger():
+    return RLogger()
+
+
+def test_checking_info(caplog, r_main_logger):
+    r_main_logger.info('salam')
+
+    assert 'salam' in caplog.text
+    assert 'INFO' in caplog.text
